d432c7f3b805369f50857420a7a265ce1e2af357
michiel-jfx <michiel@dotjava.nl>
2025-05-02 15:46:24 +0000

    cleanup and README changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module nl.dotjava.javafx.iceconverter {
+
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
 
     opens nl.dotjava.javafx.iceconverter to javafx.fxml;
     exports nl.dotjava.javafx.iceconverter;
