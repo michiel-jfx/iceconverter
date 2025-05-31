dd83be6f4263d64c3669c434b8cc1a245a633038
michiel-jfx <michiel@dotjava.nl>
2025-05-31 06:46:54 +0000

    Updated deprecated setScale() method call

--- src/main/java/nl/dotjava/javafx/support/ConvertSupport.java
+++ src/main/java/nl/dotjava/javafx/support/ConvertSupport.java
@@ -3,6 +3,7 @@ package nl.dotjava.javafx.support;
 import nl.dotjava.javafx.domain.CurrencyRate;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class ConvertSupport {
 
@@ -19,8 +20,8 @@ public class ConvertSupport {
 
     public String convertToOtherCurrency(String currencyValue) {
         try {
-            BigDecimal value = new BigDecimal(currencyValue.replace(",", ".")).setScale(3, BigDecimal.ROUND_HALF_UP);
-            return currencyRate.getCurrencySymbol() + value.multiply(currencyRate.getValueTo()).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+            BigDecimal value = new BigDecimal(currencyValue.replace(",", ".")).setScale(3, RoundingMode.HALF_UP);
+            return currencyRate.getCurrencySymbol() + value.multiply(currencyRate.getValueTo()).setScale(2, RoundingMode.HALF_UP);
         } catch (Exception e) {
             return OOPS;
         }
@@ -28,8 +29,8 @@ public class ConvertSupport {
 
     public String convertToEuroCurrency(String euroValue) {
         try {
-            BigDecimal value = new BigDecimal(euroValue.replace(",", ".")).setScale(3, BigDecimal.ROUND_HALF_UP);
-            return currencyRate.getTargetSymbol() + value.multiply(currencyRate.getValueFrom()).setScale(2, BigDecimal.ROUND_HALF_UP).toString();
+            BigDecimal value = new BigDecimal(euroValue.replace(",", ".")).setScale(3, RoundingMode.HALF_UP);
+            return currencyRate.getTargetSymbol() + value.multiply(currencyRate.getValueFrom()).setScale(2, RoundingMode.HALF_UP);
         } catch (Exception e) {
             return OOPS;
         }
